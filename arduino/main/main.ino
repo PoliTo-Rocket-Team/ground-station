5a667133e07dca87b8b0895fa34d7d6cc5fb32d2
Andrea De Masi <andreademasi80@gmail.com>
2023-03-27 17:21:53 +0200

    Arduino backend link check

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,36 +1,56 @@
+bool backend_connected = false;
+
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
-    randomSeed(analogRead(0));
-    
+  randomSeed(analogRead(0));
+  // Sending Ready Signal to Backend
+  Serial.print('R');
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int randNumber;
-  // COM CHECK
-  Serial.write('C');
+
+  // waiting for backend ready signal
+  if (backend_connected) {
+
+    int randNumber;
+    // COM CHECK
+    Serial.write('C');
     // Emulating errors
     randNumber = random(40);
-  if (randNumber > 30) {
-    Serial.write('E');
-    Serial.write('1');
-    return;
-  }
+    if (randNumber > 30) {
+      Serial.write('E');
+      Serial.write('1');
+      return;
+    }
 
-  if (randNumber > 20) {
-    Serial.write('E');
-    Serial.write('2');
-    return;
-  }
+    if (randNumber > 20) {
+      Serial.write('E');
+      Serial.write('2');
+      return;
+    }
 
-  if (randNumber > 10) {
-    Serial.write('E');
-    Serial.write('3');
-    return;
-  }
+    if (randNumber > 10) {
+      Serial.write('E');
+      Serial.write('3');
+      return;
+    }
 
-  // ALL OK
-  Serial.write('O');
-  delay(5000);
+    // ALL OK
+    Serial.write('O');
+    delay(5000);
+  }
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == 'B') {
+      backend_connected = true;
+    }
+  }
 }
