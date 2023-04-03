9eda42f336dfac0adc1f46aef94488fa770b6e13
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-04-03 13:43:15 +0200

    feat: imported font

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFont>
+#include <QFontDatabase>
 #include "antenna.h"
+
+#include <QFile>
 
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    int font_id = QFontDatabase::addApplicationFont(":/JakartaSans/Regular");
+    QFontDatabase::addApplicationFont(":/JakartaSans/Bold");
+    QFontDatabase::addApplicationFont(":/JakartaSans/SemiBold");
+    QFontDatabase::addApplicationFont(":/JakartaSans/Medium");
+    QFontDatabase::addApplicationFont(":/JakartaSans/Light");
+
+    QString family = QFontDatabase::applicationFontFamilies(font_id).at(0);
+    QFont font(family, 12);
+    app.setFont(font);
 
     QScopedPointer<Antenna> antenna(new Antenna);
 
